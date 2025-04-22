db77f1f10511d0ea1d0f1dd6445c0e39a89664c5
Harsh <hkjal1605@gmail.com>
2025-04-22 16:24:55 +0530

    Fixed padding issue

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -2,7 +2,7 @@ import PrivacyPolicy from '@/mdx/privacy-policy.mdx';
 
 export default function Page() {
   return (
-    <div className="prose prose-lg prose-invert !text-black-800 px-20 py-40 max-w-none">
+    <div className="prose prose-lg prose-invert !text-black-800 px-5 lg:px-20 py-40 max-w-none">
       <PrivacyPolicy />
     </div>
   );
