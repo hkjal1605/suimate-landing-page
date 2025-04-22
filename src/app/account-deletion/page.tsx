54ea94d815ac1ef0f13f44b4896c9797996d5b55
Harsh <hkjal1605@gmail.com>
2025-04-22 19:39:04 +0530

    Added the page for account data deletion

---
+++ src/app/account-deletion/page.tsx
@@ -0,0 +1,9 @@
+import AccountDeletion from '@/mdx/account-deletion.mdx';
+
+export default function Page() {
+  return (
+    <div className="prose prose-lg prose-invert !text-black-800 px-5 lg:px-20 py-40 max-w-none">
+      <AccountDeletion />
+    </div>
+  );
+}
